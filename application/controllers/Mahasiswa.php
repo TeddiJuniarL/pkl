137ab7a8f6d7af4fb59a04d16501ebe6f41f9b37
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-09-18 10:00:01 +0700

    Update Mahasiswa.php

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,8 +6,11 @@ class Mahasiswa extends CI_Controller
 	 function __construct()
  	{
  		parent::__construct();
- 		$this->load->helper(array('url'));
-		$this->load->model('Model_Mahasiswa');
+ 	//	$this->load->helper(array('url'));
+	//	$this->load->model('Model_Mahasiswa');
+		$this->load->model(array(
+			'Model_Mahasiswa'
+		));
  	}
 
 	function index()
@@ -19,16 +22,21 @@ class Mahasiswa extends CI_Controller
 
 	function input(){
 		if (isset($_POST['Btntambah'])){
-			$data = $this->Model_Mahasiswa->input(array (
+			$data = array(
 			'nim' => $this->input->post('nim'),
 			'nama' => $this->input->post('nama'),
 			'tm_prodi_id' => $this->input->post('prodi'),
 			'tm_gol_id' => $this->input->post('gol'),
 			'alamat' => $this->input->post('alamat'),
-			'telp' => $this->input->post('telp'),
-			'foto' => $this->input->post('foto')));
-			redirect('Mahasiswa/');
-		}else{
+			'telp' => $this->input->post('telp'),);
+
+		if (!empty($_FILES['foto']['name'])) {
+			$upload = $this->aksi_upload();
+			$data['foto'] = $upload;
+		}
+			$this->Model_Mahasiswa->input($data);
+			redirect('Mahasiswa', $data);
+	}else{
 			$x =$this->Model_Mahasiswa->get_prodi();
 			$data = array(
 				'prodi'=>$this->Model_Mahasiswa->get_prodi(),
@@ -36,8 +44,27 @@ class Mahasiswa extends CI_Controller
 				);
 			//var_dump($x);
 			$this->load->view('App/input_mhs',$data);
-		}
 	}
+}
+
+	function aksi_upload(){
+ 		$config['upload_path']          = 'assets/images/';
+		$config['allowed_types']        = 'gif|jpg|png|jpeg';
+		$config['max_size']             = 2048;
+		$config['max_width']            = 1024;
+		$config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload('foto')){
+			$error = array('error' => $this->upload->display_errors());
+			$this->load->view('App/input_mhs', $error);
+		}else{
+			$data = array('upload_data' => $this->upload->data());
+			$this->load->view('App/list_mhs', $data);
+			}
+		}
+
 
 	function delete($id){
 		$this->Model_Mahasiswa->delete($id);
